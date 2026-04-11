4bd3ebf6758d7a0d97ad972003443f57cc5a0b1f
Zdm17y4 <miguelangel20010817@gmail.com>
2026-04-11 00:52:46 -0500

    Navbar inicial de la página

---
+++ mi-react-app/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Hola</h1>
+      <h1>React</h1>
+      <p>Mi primera app en React</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ mi-react-app/src/Navbar.jsx
@@ -0,0 +1,46 @@
+function Navbar(){
+    return (
+        <nav>
+            <ul>
+                <li>PLANCHADO Y PINTURA
+                    <ul>
+                        <li>PLANCHADO Y PINTURA PAÑOS</li>
+                        <li>TRATAMIENTO DE PINTURA</li>
+                        <li>REPARACION Y PINTURA DE AROS</li>
+                        <li>PINTURA EXPRESS</li>
+                    </ul>
+                </li>
+                <li>ELECTROMOVILIDAD
+                    <ul>
+                        <li>VENTA DE VEHICULOS NUEVOS</li>
+                    </ul>
+                </li>
+                <li>MECÁNICA
+                    <ul>
+                        <li>INSPECCION Y DIAGNÓSTICO</li>
+                        <li>SERVICIO DE MANTENIMIENTOS</li>
+                        <li>FRENOS</li>
+                        <li>SUSPENSION Y DIRECCIÓN</li>
+                        <li>SISTEMA DE REFRIGERACIÓN</li>
+                        <li>ELECTRICIDAD Y ELECTRÓNICA</li>
+                        <li>AIRE ACONDICIONADO</li>
+                        <li>REPARACIÓN DE MOTORES</li>
+                    </ul>
+                </li>
+                <li>PLANCHADO
+                    <ul>
+                        <li>PLANCHADO Y PINTURA PAÑOS</li>
+                        <li>TRATAMIENTO DE PINTURA</li>
+                        <li>REPARACION Y PINTURA DE AROS</li>
+                        <li>PINTURA EXPRESS</li>
+                    </ul>
+                </li>
+            </ul>
+
+            <div>
+                <span>WHATSAPP</span>
+                <strong>977123233</strong>
+            </div>
+        </nav>
+    )
+}
