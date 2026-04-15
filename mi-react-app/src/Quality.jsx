7835ebcdb3dfd13864a912dbb713125a24d555b0
Zdm17y4 <miguelangel20010817@gmail.com>
2026-04-15 00:57:00 -0500

    Avance de los mensajes sobre la calidad del taller

---
+++ mi-react-app/src/Quality.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import './Quality.css'
+
+const Quality = () => {
+    return (
+        <section className="quality-section">
+            <div className="quality-grid">
+                <div className="quality-premium">
+                    <img src="https://www.cocacolaep.com/assets/legacy-assets/Uploads/resources/Coca-Cola-1210__FocusFillWyIwLjAwIiwiMC4wMCIsMTM3Niw1MzJd.jpg" alt="logo-premium" />
+                    <h2>AUTOS Y CAMIONETAS DE ALTA GAMA</h2>
+                    <div className="list-container">
+                        <ul>
+                            <li>✔ BMW</li>
+                            <li>✔ Mercedes-Benz</li>
+                            <li>✔ Audi</li>
+                            <li>✔ Porsche</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="quality-normal">
+                    <img src="https://www.cocacolaep.com/assets/legacy-assets/Uploads/resources/Coca-Cola-1210__FocusFillWyIwLjAwIiwiMC4wMCIsMTM3Niw1MzJd.jpg" alt="logo-normal" />
+                    <h2>AUTOS Y CAMIONETAS MULTIMARCA</h2>
+                    <div className="quality-normal-grid">
+                        <div className="list-column">
+                            <ul>
+                                <li>✔ Kia</li>
+                                <li>✔ Hyundai</li>
+                                <li>✔ Mazda</li>
+                                <li>✔ Volkswagen</li>
+                                <li>✔ Toyota</li>
+                                <li>✔ Nissan</li>
+                                <li>✔ Mitsubishi</li>
+                                <li>✔ Mini</li>
+                                <li>✔ Seat</li>
+                            </ul>
+                        </div>
+                        <div className="list-column">
+                            <ul>
+                                <li>✔ Suzuki</li>
+                                <li>✔ Chevrolet</li>
+                                <li>✔ Honda</li>
+                                <li>✔ Ford</li>
+                                <li>✔ Jeep</li>
+                                <li>✔ Dodge</li>
+                                <li>✔ Renault</li>
+                                <li>✔ Peugeot</li>
+                                <li>✔ Citröen</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Quality;
