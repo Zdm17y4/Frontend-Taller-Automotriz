bff4ef851fffc25050f5ad05994112e7880f0e26
Zdm17y4 <miguelangel20010817@gmail.com>
2026-04-16 16:24:57 -0500

    Agregado de logos de autos

--- mi-react-app/src/Recognitions.jsx
+++ mi-react-app/src/Recognitions.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
+import porscheLogo from './assets/logos/porsche.png'
+import mercedesbenzLogo from './assets/logos/mercedesbenz.png'
+import bmwLogo from './assets/logos/bmw.png'
 import './Recognitions.css'
+
 const Recognitions = () => {
     return (
         <section className='recognitions-section'>
             <h3>RECONOCIDOS POR</h3>
             <div className='recognitions-grid'>
                 <div className='recognitions-item'>
-                    <img src="https://logo-teka.com/wp-content/uploads/2025/06/porsche-crest-logo.png" alt="Logo" />
+                    <img src={porscheLogo} alt="Logo" />
                     <p>"MEJOR SOCIO ESTRATÉGICO" DE INCHCAPE</p>
                 </div>
                 <div className='recognitions-item'>
-                    <img src="https://logo-teka.com/wp-content/uploads/2025/06/porsche-crest-logo.png" alt="Logo" />
+                    <img src={mercedesbenzLogo} alt="Logo" />
                     <p>INCORPORADO AL PROGRAMA "THE BEST ALLIANCE".</p>
                 </div>
                 <div className='recognitions-item'>
-                    <img src="https://logo-teka.com/wp-content/uploads/2025/06/porsche-crest-logo.png" alt="Logo" />
+                    <img src={bmwLogo} alt="Logo" />
                     <p>"EMPRESA PERUANA DEL AÑO" POR 4 AÑOS CONSECUTIVOS.</p>
                 </div>
 
