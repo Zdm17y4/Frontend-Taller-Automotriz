4c63494a469fbbc8d0ee27280af8c7bf9e5e04f5
Zdm17y4 <miguelangel20010817@gmail.com>
2026-04-16 16:23:25 -0500

    Agregado de imagenes con cambios de color en el css

--- mi-react-app/src/Features.jsx
+++ mi-react-app/src/Features.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import feature1 from './assets/icon/feature-1.png'
+import feature2 from './assets/icon/feature-2.png'
+import feature3 from './assets/icon/feature-3.png'
+import feature4 from './assets/icon/feature-4.png'
 import './Features.css';
 const Features = () => {
     return (
@@ -6,19 +10,19 @@ const Features = () => {
             <h2>EL MEJOR TALLER DE LIMA CON LOS MEJORES PRECIOS</h2>
             <div className="features-grid">
                 <div className="feature-item">
-                    <img src='https://www.germania.com.pe/app/views/front_end/tnw_front_1/imgs/icons/trofeo.png' alt="Trofeo" />
+                    <img src={feature1} alt="Trofeo" />
                     <p><b>55</b> Años de experiencia en reparación de autos</p>
                 </div>
                 <div className="feature-item">
-                    <img src='https://www.germania.com.pe/app/views/front_end/tnw_front_1/imgs/icons/trofeo.png' alt="Trofeo" />
+                    <img src={feature2} alt="Trofeo" />
                     <p><b>4</b> Locales ubicados en Lima</p>
                 </div>
                 <div className="feature-item">
-                    <img src='https://www.germania.com.pe/app/views/front_end/tnw_front_1/imgs/icons/trofeo.png' alt="Trofeo" />
+                    <img src={feature3} alt="Trofeo" />
                     <p>Trabajamos con todas las compañías de seguros</p>
                 </div>
                 <div className="feature-item">
-                    <img src='https://www.germania.com.pe/app/views/front_end/tnw_front_1/imgs/icons/trofeo.png' alt="Trofeo" />
+                    <img src={feature4} alt="Trofeo" />
                     <p>Más de <b>70</b> trabajadores altamente capacitados</p>
                 </div>
             </div>
