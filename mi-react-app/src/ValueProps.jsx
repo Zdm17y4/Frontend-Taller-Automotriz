68e6719ccbdbe433e43d64bcc9550d1f6f2c0187
Zdm17y4 <miguelangel20010817@gmail.com>
2026-04-16 16:25:46 -0500

    Agregado de imagenes de mejor calidad

--- mi-react-app/src/ValueProps.jsx
+++ mi-react-app/src/ValueProps.jsx
@@ -1,19 +1,22 @@
 import React from "react";
+import pintura from './assets/images/pintura.png'
+import planchado from './assets/images/planchado.png'
+import atencion from './assets/images/atencion.png'
 import './ValueProps.css'
 const ValueProps = () => {
     return (
         <section className="valueprops-section">
             <div className="valueprops-grid">
                 <div className="valueprops-item">
-                    <img src="https://www.germania.com.pe/app/views/front_end/tnw_front_1/imgs/horno.jpg" alt="photos" />
+                    <img src={pintura} alt="photos" />
                     <p>Pintura original de fábrica</p>
                 </div>
                 <div className="valueprops-item">
-                    <img src="https://www.germania.com.pe/app/views/front_end/tnw_front_1/imgs/horno.jpg" alt="photos" />
+                    <img src={planchado} alt="photos" />
                     <p>3 años de garantía en planchado y pintura</p>
                 </div>
                 <div className="valueprops-item">
-                    <img src="https://www.germania.com.pe/app/views/front_end/tnw_front_1/imgs/horno.jpg" alt="photos" />
+                    <img src={atencion} alt="photos" />
                     <p>Atención rápida y personalizada</p>
                 </div>
             </div>
