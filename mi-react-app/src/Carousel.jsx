c6ecb5fe7be530224092ce006442e9b71e360058
Zdm17y4 <miguelangel20010817@gmail.com>
2026-04-16 16:20:43 -0500

    Agregado de imagenes, ligera modificacion en la estructura y diseño

--- mi-react-app/src/Carousel.jsx
+++ mi-react-app/src/Carousel.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import './Carousel.css';
 
-const Carousel = ({ items}) => {
+const Carousel = ({ items }) => {
     const [currentIndex, setCurrentIndex] = useState(0);
     const nextSlide = () => {
         setCurrentIndex((prevIndex) =>
@@ -13,14 +13,25 @@ const Carousel = ({ items}) => {
             prevIndex === 0 ? items.length - 1 : prevIndex - 1
         );
     };
+    
+    const currentItem = items[currentIndex];
+    
     return (
         <div className='carousel-container'>
             <button className='carousel-arrow left' onClick={prevSlide}>
                 ❮
             </button>
 
-            <div className='carousel-slide'>
-                {items[currentIndex]}
+            <div 
+                className='carousel-slide'
+                style={{
+                    backgroundImage: `url(${currentItem.image})`
+                }}
+            >
+                <div className='carousel-slide-content'>
+                    <h2>{currentItem.title}</h2>
+                    <p>{currentItem.subtitle}</p>
+                </div>
             </div>
 
             <button className='carousel-arrow right' onClick={nextSlide}>
