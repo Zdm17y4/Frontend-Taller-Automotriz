a467e247549a9e22a7509bfc1f9eb355af43a025
Zdm17y4 <miguelangel20010817@gmail.com>
2026-04-16 16:16:48 -0500

    Agregar seccion de copyright

---
+++ mi-react-app/src/Copyright.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Copyright.css'
+
+const Copyright = () => {
+    return (
+        <section className="copyright-section">
+            <div className="copyright-container">
+                <p className="copyright-text">
+                    TALLER AUTOMOTRIZ | EL MEJOR TALLER DE MECÁNICA, PLANCHADO Y PINTURA EN LIMA. 2026 - Todos los derechos reservados.
+                </p>
+            </div>
+        </section>
+    )
+}
+export default Copyright;
